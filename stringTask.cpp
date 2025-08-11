9ac8fbdb85071015749ed8d021d40ffd5979979d
Test_Case <badhonpain48@gmail.com>
2025-08-11 19:36:38 +0600

    solved 118A- String Task

--- stringTask.cpp
+++ stringTask.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <cctype>
 using namespace std;
 int main()
@@ -8,13 +9,20 @@ int main()
     for (int i = 0; i < s.length(); i++)
     {
         s[i] = tolower(s[i]);
-        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
-        {
-           s.erase(i, 1);
-        }
-        else
-        
-        
     }
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y')
+        {
+            s.erase(i, 1);
+            i--;
+        }
+    }
+
+    for (int i = s.length() - 1; i >= 0; i--)
+    {
+        s.insert(i, ".");
+    }
+
     cout << s << endl;
 }
