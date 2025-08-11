eef597b68e18d2c6e43a79880eea6c1a58bb6073
Test_Case <badhonpain48@gmail.com>
2025-08-11 10:44:12 +0600

    solved 520A pangram

--- hulk.cpp
+++ hulk.cpp
@@ -2,7 +2,28 @@
 using namespace std;
 int main()
 {
-string h=" hate ", l=" love ";
+string one= "I hate", two=" that I love", three = " that I hate", bonus = " it";
+int n;
+cin >> n;
+if (n==1)
+{
+   one.append(bonus);
+   cout<< one << endl;
+}
+else if (n==2)
+{
+   one.append(two).append(bonus);
+   cout<< one << endl;
+}
+else if (n==3)
+{
+   one.append(two).append(three).append(bonus);
+   cout<< one << endl;
+}
+
+
+
+
 
 
 
