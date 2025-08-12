4c844fcdacc497e10628281d5b1e6d29292e5050
Test_Case <badhonpain48@gmail.com>
2025-08-13 03:37:07 +0600

    solved 1560A Dislike of Threes

--- dislike_the_threes.cpp
+++ dislike_the_threes.cpp
@@ -1,12 +1,29 @@
-#include<iostream>
-#include<string>
-
+#include <iostream>
 using namespace std;
-int main()
-{
+
+int main() {
     int test;
     cin >> test;
-    int arr[test];
     
-
+    for (int i = 0; i < test; i++) {
+        int k;
+        cin >> k;
+        
+        int count = 0;
+        int current_number = 1;
+        
+        while (true) {
+            
+            if (current_number % 3 != 0 && current_number % 10 != 3) {
+                count++;
+                if (count == k) {
+                    cout << current_number << endl;
+                    break;
+                }
+            }
+            current_number++;
+        }
+    }
+    
+    return 0;
 }
