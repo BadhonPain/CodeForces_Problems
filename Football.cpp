30d2132e4efef6183f2567944915566eecfa5249
Test_Case <badhonpain48@gmail.com>
2025-08-14 04:16:47 +0600

    solved 96A-Football

---
+++ Football.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int count = 1;
+    string s;
+    getline(cin, s);
+    for (int i = 1; i < s.length(); i++)
+    {
+
+        if (s[i] == s[i - 1])
+        {
+            count++;
+            if (count == 7)
+            {
+                cout << "YES" << endl;
+                return 0;
+            }
+        }
+        else
+            count = 1;
+    }
+    if (count != 7)
+    {
+        cout << "NO" << endl;
+    }
+}
