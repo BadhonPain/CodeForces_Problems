c2c34161498f615e4e1800855dd2c7187007211b
Test_Case <badhonpain48@gmail.com>
2025-08-20 05:06:59 +0600

    1971A - My First Sorting Problem

---
+++ My_first_sorting_problem.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a > b)
+        {
+            cout << b << " " << a << endl;
+        }
+        else
+            cout << a << " " << b << endl;
+    }
+}
