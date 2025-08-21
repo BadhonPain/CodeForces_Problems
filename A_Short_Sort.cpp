781f5121ed3b5497d072a2e46e880f271959dcb7
Test_Case <badhonpain48@gmail.com>
2025-08-21 07:35:22 +0600

    solved 1873A - Short Sort

---
+++ A_Short_Sort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+ 
+    while (t--)
+    {
+        string s;
+        cin >> s;
+ 
+        int count = 0;
+        if (s[0] != 'a')
+            count++;
+        if (s[1] != 'b')
+            count++;
+        if (s[2] != 'c')
+            count++;
+ 
+        if (count <= 2)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
