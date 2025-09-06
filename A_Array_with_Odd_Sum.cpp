3746bd63850a8c8b3b4f60cecd6487da11eb3267
Test_Case <badhonpain48@gmail.com>
2025-09-06 07:42:14 +0600

    solved : 2044B - Normal Problem

---
+++ A_Array_with_Odd_Sum.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            sum += a[i];
+        }
+        if (sum % 2 == 0)
+        {
+            cout << "NO" << endl;
+        }
+        else
+            cout << "YES" << endl;
+    }
+}
