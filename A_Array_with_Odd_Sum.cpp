812cb4692bacf771664aae79cd4ab8a64b618aaa
Test_Case <badhonpain48@gmail.com>
2025-12-11 00:31:32 +0600

    solved 1296A - Array with Odd Sum

--- A_Array_with_Odd_Sum.cpp
+++ A_Array_with_Odd_Sum.cpp
@@ -1,25 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
     int test;
     cin >> test;
     while (test--)
     {
-        int n;
-        cin >> n;
-        int a[n];
-        int sum = 0;
-        for (int i = 0; i < n; i++)
+        int size;
+        cin >> size;
+        int a[size];
+        int oddCount = 0, evenCount = 0;
+        for (int i = 0; i < size; i++)
         {
             cin >> a[i];
-            sum += a[i];
+            if (a[i] % 2 == 0)
+            {
+                evenCount++;
+            }
+            else
+                oddCount++;
         }
-        if (sum % 2 == 0)
+
+        if (oddCount == 0)
         {
             cout << "NO" << endl;
         }
-        else
-            cout << "YES" << endl;
+        else if (oddCount)
+        {
+            if (!evenCount && (size % 2 == 0))
+            {
+                cout << "NO" << endl;
+            }
+            else
+                cout << "YES" << endl;
+        }
     }
 }
