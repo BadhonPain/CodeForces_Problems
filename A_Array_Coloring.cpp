5d20fa77c01d4a414588d571f5618591516d8cd6
Test_Case <badhonpain48@gmail.com>
2025-12-24 18:43:50 +0600

    solved: 1857A - Array Coloring

---
+++ A_Array_Coloring.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+            sum += arr[i];
+        }
+        if (sum % 2 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+            cout << "NO" << endl;
+    }
+}
