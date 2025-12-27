048985a58586ccbd821e053ab51f7ead18b7c68a
Test_Case <badhonpain48@gmail.com>
2025-12-28 01:05:47 +0600

    solved: 1370A- Maximum GCD

---
+++ A_Maximum_GCD.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int n;
+        cin>>n;
+        cout<< floor(n/2.0)<<endl;
+    }
+}
