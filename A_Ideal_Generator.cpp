b843907869f8cd5e50c2c179ebdfdf206b734789
Test_Case <badhonpain48@gmail.com>
2025-12-30 00:41:53 +0600

    solved: 2093A- Ideal Generator

---
+++ A_Ideal_Generator.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test;
+    cin >> test;
+    while (test--)
+    {
+        int n;
+        cin >> n;
+        (n % 2 == 0) ? cout << "NO" << endl : cout << "YES" << endl;
+    }
+}
