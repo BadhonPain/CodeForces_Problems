8213522b35020498523cacebd4b3f3190e5429a4
Test_Case <badhonpain48@gmail.com>
2026-01-02 01:55:58 +0600

    unsolved:189A

--- A_Cut_Ribbon.cpp
+++ A_Cut_Ribbon.cpp
@@ -4,5 +4,16 @@ int main()
 {
     int n, a, b, c;
     cin >> n >> a >> b >> c;
-    cout << ceil((a + b + c) / (float(n))) << endl;
+    int max_count = INT_MIN;
+    if (n % a == 0)
+        max_count = max(max_count, n / a);
+    if (n % b == 0)
+        max_count = max(max_count, n / b);
+    if (n % c == 0)
+        max_count = max(max_count, n / c);
+    if (n == a + b + c)
+        max_count = max(max_count, 3);
+    else if (n == a + b || n == a + c || n == b + c)
+        max_count = max(max_count, 2);
+    cout << max_count << endl;
 }
